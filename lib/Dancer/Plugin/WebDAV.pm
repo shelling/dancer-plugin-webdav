2dfe836f438a43e614b0839db63b1c3df8232470
shelling <navyblueshellingford@gmail.com>
2012-11-11 19:42:43 +0800

    initial commit

---
+++ lib/Dancer/Plugin/WebDAV.pm
@@ -0,0 +1,32 @@
+package Dancer::Plugin::WebDAV;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Dancer::Plugin::WebDAV -
+
+=head1 SYNOPSIS
+
+  use Dancer::Plugin::WebDAV;
+
+=head1 DESCRIPTION
+
+Dancer::Plugin::WebDAV is
+
+=head1 AUTHOR
+
+shelling E<lt>navyblueshellingford@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
